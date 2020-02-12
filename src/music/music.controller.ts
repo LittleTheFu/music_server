362b8a1e1aa65a4ea8ea2ecf7769719a2dbf5957
fuyuxiang <lttlstpd@live.com>
2020-02-12 14:14:38 +0800

    format code

--- src/music/music.controller.ts
+++ src/music/music.controller.ts
@@ -1,19 +1,18 @@
 import { Controller, Get, Res, Header } from '@nestjs/common';
 import { MusicService } from './music.service';
 
-
 @Controller('music')
 export class MusicController {
-    constructor(private readonly musicService: MusicService) {}
+  constructor(private readonly musicService: MusicService) {}
 
-    // @Get()
-    // findAll(): object {
-    //     return {'name' : 'http://localhost:9999/public/1.mp3'};
-    // }
+  // @Get()
+  // findAll(): object {
+  //     return {'name' : 'http://localhost:9999/public/1.mp3'};
+  // }
 
-    @Get()
-    // @Header('Content-Type', 'audio/mpeg')
-    findAll(@Res() res):  Promise<void>{
-        return res.sendFile('1.mp3', { root: './public'});
-    }
+  @Get()
+  // @Header('Content-Type', 'audio/mpeg')
+  findAll(@Res() res): Promise<void> {
+    return res.sendFile('1.mp3', { root: './public' });
+  }
 }
