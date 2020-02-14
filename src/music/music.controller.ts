74dce77f3c52cda6d69ca3b7927b08c493ba99c6
fuyuxiang <lttlstpd@live.com>
2020-02-14 15:33:08 +0800

    a route for next song

--- src/music/music.controller.ts
+++ src/music/music.controller.ts
@@ -1,18 +1,24 @@
-import { Controller, Get, Res, Header } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { MusicService } from './music.service';
 
 @Controller('music')
 export class MusicController {
-  constructor(private readonly musicService: MusicService) {}
+  constructor(private readonly musicService: MusicService) {
+  }
 
   // @Get()
   // findAll(): object {
   //     return {'name' : 'http://localhost:9999/public/1.mp3'};
   // }
-
+  
   @Get()
   // @Header('Content-Type', 'audio/mpeg')
   findAll(@Res() res): Promise<void> {
     return res.sendFile('1.mp3', { root: './public' });
+  }
+
+  @Get('next')
+  getNextMusic(@Res() res): Promise<void> {
+    return res.sendFile('2.mp3', { root: './public' });
   }
 }
