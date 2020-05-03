b397e881110571327d965f618b780310d363a0ca
fuyuxiang <lttlstpd@live.com>
2020-05-03 14:47:11 +0800

    clean code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import { NestFactory, APP_FILTER } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
